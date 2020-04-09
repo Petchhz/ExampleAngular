9d82552be607b93ca99aea9e63375098d444837a
Petchhz <petchz2541@gmail.com>
2020-04-09 22:26:26 +0700

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router'
+import {ReactiveFormsModule} from '@angular/forms' 
 
+import { TopBarComponent } from './top-bar/top-bar.component';
+import {ProductListComponent} from './product-list/product-list.component'
 import { AppComponent } from './app.component';
+import {ProductAlertsComponent} from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,TopBarComponent,ProductListComponent,ProductAlertsComponent,
+    ProductDetailsComponent,CartComponent,ShippingComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,HttpClientModule,ReactiveFormsModule,RouterModule.forRoot([
+        { path: '', component: ProductListComponent },
+        { path: 'products/:productId', component: ProductDetailsComponent },
+        { path: 'cart', component: CartComponent },
+        { path: 'shipping', component: ShippingComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
